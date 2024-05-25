d82a98d06009c7b3ebfa243e9e40e8331de5ec9b
Zin Thandar Hlaing <zinthandarhlaing@Zins-MacBook-Pro-14.local>
2024-05-25 20:33:11 +0800

    [feat]: set sample result

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,51 @@
 
 import ExtractedResultContent from "@/components/ExtractedResultContent";
 import ImageUploadForm from "@/components/ImageUploadForm";
+import { useAppDispatch, useAppSelector } from "@/hooks/redux";
+import { uploadImage } from "@/store/application/application.slice";
+import { extractColors } from "extract-colors";
+import { FinalColor } from "extract-colors/lib/types/Color";
+import { useEffect } from "react";
 import styles from "../styles/page.module.css";
 
 export default function Home() {
-  return (
-    <>
-      <main className={styles.main}>
-        <div className={styles.container}>
-          <h1>Pixel Palette Picker</h1>
+  const dispatch = useAppDispatch();
+  const { imagePreviewUrl } = useAppSelector((state) => state.application);
 
-          <div className={styles.grid}>
-            <div className={styles.griditem}>
-              <div className={styles.card}>
-                <ImageUploadForm />
-              </div>
+  useEffect(() => {
+    if (!imagePreviewUrl) {
+      generateSampleColors();
+    }
+  }, []);
+
+  const generateSampleColors = () => {
+    var src = "sample.jpg";
+
+    extractColors(src)
+      .then((data: FinalColor[]) => {
+        dispatch(uploadImage({ url: src, colors: data }));
+      })
+      .catch(console.error);
+  };
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <h1>Pixel Palette Picker</h1>
+
+        <div className={styles.grid}>
+          <div className={styles.griditem}>
+            <div className={styles.card}>
+              <ImageUploadForm />
             </div>
-            <div className={styles.griditem}>
-              <div className={styles.card}>
-                <ExtractedResultContent />
-              </div>
+          </div>
+          <div className={styles.griditem}>
+            <div className={styles.card}>
+              <ExtractedResultContent />
             </div>
           </div>
         </div>
-      </main>
-    </>
+      </div>
+    </div>
   );
 }
